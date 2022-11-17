f84882618ddd88ea180bb1a4ebd188039544afa1
József Mandula <jozsef.mandula@gmail.com>
2022-11-17 04:29:24 +0100

    cms fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,3 @@
-// import CMS from "netlify-cms-app";
-// // Initialize the CMS object
-// CMS.init();
-// // Now the registry is available via the CMS object.
-// CMS.registerPreviewTemplate("my-template", MyTemplate);
-
 const fs = require("fs");
 
 const { DateTime } = require("luxon");
